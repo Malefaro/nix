d4fa8977c98ae2b46a7d9318b29f5c889d473b19
Danil Anokhin <malefaro@gmail.com>
2021-12-18 02:00:34 +0300

    readmi, zsh, gcc

--- home.nix
+++ home.nix
@@ -6,10 +6,12 @@
 
   home = {
     packages = with pkgs; [
+      zsh
       neovim
       git
       ripgrep
       tmux
+      gcc
       alacritty
       (nerdfonts.override {
         fonts = [
@@ -81,5 +83,54 @@
       ".DS_Store"
       "*.pyc"
     ];
+  };
+
+  programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+#            "command-not-found"
+#            "poetry"
+        ];
+        theme = "agnoster";
+      };
+      plugins = [
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "2d60a47cc407117815a1d7b331ef226aa400a344";
+            sha256 = "1pnxr39cayhsvggxihsfa3rqys8rr2pag3ddil01w96kw84z4id2";
+          };
+        }
+      ];
+      localVariables = {
+        EDITOR = "nvim";
+      };
+      initExtra = ''
+        unset LESS
+
+        export GOPATH=~/GoLang
+        export GOBIN=$GOPATH/bin
+        export GOPROXY=""
+        export PYENV_ROOT="$HOME/.pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+        eval "$(pyenv init --path)"
+
+        export PATH="$HOME/.poetry/bin:$PATH"
+        export PATH="$GOBIN:$PATH"
+        export PATH="$HOME/.local/bin:$PATH"
+
+        eval "$(op completion zsh)"; compdef _op op
+
+        alias hms="home-manager switch"
+
+        source $HOME/.exports
+      '';
   };
 }
