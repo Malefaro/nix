0bbad6c92642256492b78a1b8307509460ff5494
Danil Anokhin <malefaro@gmail.com>
2022-03-01 04:52:41 +0300

    fix rust

--- config/nvim/lua/lsp_setup.lua
+++ config/nvim/lua/lsp_setup.lua
@@ -67,12 +67,21 @@ lsp_installer.on_server_ready(function(server)
 		opts.cmd = {"gopls"} -- gopls installed in gobin path
 	end
     if server.name =="rust_analyzer" then
-
-        require('rust-tools').setup({ server = {
-            on_attach = on_attach,
-            cmd=server._default_options.cmd,
-            capabilities = capabilities,
-        } })
+        require("rust-tools").setup {
+            -- The "server" property provided in rust-tools setup function are the
+            -- settings rust-tools will provide to lspconfig during init.            -- 
+            -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+            -- with the user's own settings (opts).
+            server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+        }
+        server:attach_buffers()
+        -- Only if standalone support is needed
+        require("rust-tools").start_standalone_if_required()
+        -- require('rust-tools').setup({ server = {
+        --     on_attach = on_attach,
+        --     cmd=server._default_options.cmd,
+        --     capabilities = capabilities,
+        -- } })
         return
     end
 
